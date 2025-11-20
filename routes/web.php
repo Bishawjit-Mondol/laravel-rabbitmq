09e21754376b75123a9ebe2e8efed4dc6a4a7ae6
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2025-11-20 21:11:59 +0600

    Add route for sending email jobs to RabbitMQ. it iis working as expected

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\SendEmailJob;
+
+Route::get('/send', function () {
+    SendEmailJob::dispatch('user@example.com');
+    return "Job pushed to RabbitMQ!";
+});
 
 Route::get('/', function () {
     return view('welcome');
