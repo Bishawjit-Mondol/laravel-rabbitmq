d93ddb874b024a17ff000ea6e6cfad971d856c8b
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2025-11-20 21:18:38 +0600

    Enhance email job dispatching and logging functionality in RabbitMQ routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,35 @@ use Illuminate\Support\Facades\Route;
 use App\Jobs\SendEmailJob;
 
 Route::get('/send', function () {
-    SendEmailJob::dispatch('user@example.com');
-    return "Job pushed to RabbitMQ!";
+    $startTime = microtime(true);
+
+    for ($i = 1; $i <= 100; $i++) {
+        SendEmailJob::dispatch("user{$i}@example.com");
+    }
+
+    $endTime = microtime(true);
+    $executionTime = round(($endTime - $startTime) * 1000, 2);
+
+    return "100 jobs pushed to RabbitMQ in {$executionTime}ms!";
+});
+
+Route::get('/logs', function () {
+    $logFile = storage_path('logs/laravel.log');
+
+    if (!file_exists($logFile)) {
+        return "No log file found.";
+    }
+
+    $logs = file_get_contents($logFile);
+    $emailLogs = collect(explode("\n", $logs))
+        ->filter(fn($line) => str_contains($line, 'Sending email to:'))
+        ->take(100)
+        ->values();
+
+    return response()->json([
+        'total_emails_logged' => $emailLogs->count(),
+        'logs' => $emailLogs
+    ]);
 });
 
 Route::get('/', function () {
