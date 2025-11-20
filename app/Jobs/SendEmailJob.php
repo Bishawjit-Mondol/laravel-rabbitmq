9376a4257d237839a67a223c2d09e1b97ad49a6c
Bishawjit-Mondol <bishawjitmondol29@gmail.com>
2025-11-20 20:49:26 +0600

    Add SendEmailJob class for handling email sending in the queue

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $email;
+
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    public function handle()
+    {
+        Log::info("Sending email to: {$this->email}");
+    }
+}
